ea0cb1a8fdff349d9cbc44460377cfe4b7d4724b
mirajehossain <mdalamin8521@gmail.com>
2018-12-15 03:45:39 +0600

    add from for messages and show messages in the browser

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ io.on('connection',(socket)=>{
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
 
-    socket.on('createMessage', (message)=>{
-        console.log(message)
+    socket.on('createMessage', (message, callback)=>{
+        console.log(message);
         io.emit('newMessage', generateMessage(message.from, message.text));
-
+        callback('This is from the server');
     });
 
     socket.on('disconnect',()=>{
--- public/js/index.js
+++ public/js/index.js
@@ -5,8 +5,24 @@ socket.on('connect', function() {
 
 socket.on('newMessage', function (message) {
     console.log(message);
+    let li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', function() {
     console.log("disconnected from server")
+});
+
+
+
+jQuery('#message-form').on('submit',function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    })
 });
